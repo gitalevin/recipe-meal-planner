d370f997e3eef602a82a1e49b6a8538a2f81babc
gitalevin <reachglevin@gmail.com>
2022-11-06 13:51:49 -0600

    customized json for pantry_items controller actions,and chaged code to use templates rather than json and installed jb gem

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,11 +1,11 @@
 class PantryItemsController < ApplicationController
   def index
-    pantry_items = PantryItem.all
-    render json: pantry_items.as_json
+    @pantry_items = PantryItem.all
+    render template: "pantry_items/index"
   end
 
   def create
-    pantry_item = PantryItem.create(
+    @pantry_item = PantryItem.create(
       ingredient_id: params[:ingredient_id],
       user_id: params[:user_id],
       amount: params[:amount],
